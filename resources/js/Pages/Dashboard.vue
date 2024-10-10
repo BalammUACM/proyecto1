3ba4c67c8b835982970c439de373554c63e1bb1f
BalammUACM <144510465+BalammUACM@users.noreply.github.com>
2024-10-10 00:19:00 -0600

    primer brazo web

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,36 +4,47 @@ import Welcome from '@/Components/Welcome.vue';
 
 import { ref, onMounted } from 'vue';
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const target = ref();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(1350, 520);
+renderer.setSize(1350,520);//520
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
+camera.position.z = 1;
 
 function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
-
+animate();
 onMounted(() => {
   target.value.appendChild(renderer.domElement);
   animate();
 });
 
+
+const loader = new GLTFLoader();
+loader.load(
+    'models/glb/brazo_v3.glb',
+    function (gltf) {
+    scene.add(gltf.scene);
+    gltf.animations; // Array<THREE.AnimationClip>
+    gltf.scene; // THREE.Group
+    gltf.scenes; // Array<THREE.Group>
+    gltf.cameras; // Array<THREE.Camera>
+    gltf.asset; // Object
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+    console.log('An error happened');
+    }
+ );
 </script>
 
 <template>
@@ -102,5 +113,16 @@ onMounted(() => {
                     </div>
             </form>
         </div>    
-    </AppLayout>
+    </AppLayout>  
 </template>
+
+<script>
+function closeOverlay() {
+    const overlay = document.getElementById('overlayPanel');
+    overlay.style.transform = 'translateX(100%)'; // Oculta el panel deslizándolo fuera de la vista
+}
+function abrirOverlay(){
+    const overlay = document.getElementById('overlayPanel');
+    overlay.style.transform = 'translateX(0%)'; // aparece el panel deslizándolo a la vista
+}
+</script>
